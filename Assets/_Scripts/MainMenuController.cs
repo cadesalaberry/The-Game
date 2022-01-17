7b3efdfda2945df74226fbda795a6e904a9b3be5
C-A de Salaberry <cadesalaberry@yahoo.com>
2022-01-18 00:36:20 +0100

    chore(menu): :alembic: adding simple menu

---
+++ Assets/_Scripts/MainMenuController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void options()
+    {
+
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
